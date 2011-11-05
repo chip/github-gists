0ab182226c38052a46cdb29ea10a67bb342bd29f
Chip Castle <chip@chipcastle.com>
2011-11-04 20:52:20 -0500

    Add PhoneGap project files

---
+++ Github-Gists/Github-Gists/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Github-Gists
+//
+//  Created by Chip Castle on 11/4/11.
+//  Copyright Chip Castle Dot Com, Inc. 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+#Github Gists
+
+This project is for viewing your Gists on Github
+
+Tools used:
+
+* jQuery Mobile
+* PhoneGap
